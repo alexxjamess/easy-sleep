e77551d091b373acfd14e4243ef98c097033aeec
alexxjamess <alex.jamess@live.co.uk>
2021-09-22 11:22:59 +0000

    add functions to imput and validate user entering total daily advertising cost

--- run.py
+++ run.py
@@ -20,7 +20,7 @@ def get_sale_data():
     """
     Get daily sales total from user.
     Run a while loop to collect a valid data string via the the terminal, 
-    which must be a number greater or to zero. 
+    which must be a number greater or equal to zero. 
     The loop will repeatedly request data unitl it is valid
     """
     while True:
@@ -35,7 +35,7 @@ def get_sale_data():
             break
 
     return data_str
-    print(data_str)
+    
 
 def validate_data(data_str):
     """
@@ -56,3 +56,44 @@ def validate_data(data_str):
     return True    
 
 get_sale_data()
+
+def get_advertising_data():
+    """
+    Get daily Advertising total from user.
+    Run a while loop to collect a valid data string via the the terminal, 
+    which must be a number greater or equal to zeroand less than 175. 
+    The loop will repeatedly request data unitl it is valid
+    """
+    while True:
+        print(f'Please enter the total advertising cost for {today}')
+        print('Number of Advertising Cost Should be Greater Than or Equal to 0 and Less than 175')
+        print('Example: 50 OR 153 \n')
+
+        data_str_advertising = input('Enter Todays Total Advertising Cost:\n')
+        
+        if validate_data_adverstising(data_str_advertising):
+            print("Data is valid!")
+            break
+
+    return data_str_advertising
+    
+
+def validate_data_adverstising(data_str_advertising):
+    """
+    Inside the try, converts all string values into integers.
+    Raises ValueError if strings cannot be converted into int,
+    or if the value is less than 0 and geater than 175
+    """
+    try:
+        data_adverstising = int(data_str_advertising) 
+        if data_adverstising > 175:
+            raise ValueError(
+                f"Figure greater than 0 and less than 175 required, you provided {data_str_advertising}"
+            )
+    except ValueError as e:
+        print(f"Invalid data: {e}, please enter a valid number.\n")
+        return False
+
+    return True   
+
+get_advertising_data()
